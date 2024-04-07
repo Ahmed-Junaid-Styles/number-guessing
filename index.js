a096bdb48d31dba4fa112167daedb88c2c78b3c1
Ahmed-Junaid-Styles <ahmedjunaidstyles@gmail.com>
2024-04-07 08:02:27 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const ans = await inquirer.prompt([
+    { name: "guessed_number", type: "number", message: "Guess a number between 1-6: " }
+]);
+var random_number = Math.floor(Math.random() * 6) + 1;
+if (ans.guessed_number == random_number) {
+    console.log(`Congratulation! You guessed the exact number i.e ${random_number}`);
+}
+else {
+    console.log(`Incorrect guess, Please try again`);
+}
